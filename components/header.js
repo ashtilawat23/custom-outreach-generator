8fa1729a6ab59b6cddc1dab6f66bebbed757259b
ashtilawat23 <ashtilawat23@gmail.com>
2022-12-23 14:19:47 -0500

    header v1 done

--- components/header.js
+++ components/header.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import Box from '@mui/material/Box';
 import styles from '../styles/Header.module.css'
+import { Box, Button } from '@mui/material';
+import Image from 'next/image';
 
 const Header = () => {
   return (
     <Box className={styles.container}>
-      <h2>header</h2>
+      <Box className={styles.imagecontainer}>
+        <Image
+          src='/custom.png'
+          alt='customoutreach.ai'
+          fill
+          objectFit='contain'
+        />
+      </Box>
+      <Box className={styles.navlinks}>
+        <Button variant='text' className={styles.button}>Home</Button>
+        <Button variant='text' className={styles.button}>About</Button>
+        <Button variant='text' className={styles.button}>Contact</Button>
+      </Box>
     </Box>
   )
 }
